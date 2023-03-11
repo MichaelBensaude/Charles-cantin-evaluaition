4bcbf2aa61e7483ba3850f702e1ed1dc376a40ce
Michaël Bensaude <77800651+MichaelBensaude@users.noreply.github.com>
2023-03-11 02:47:10 +0100

    update contactJs
    Add function Netlify Form

--- src/view/Contact.js
+++ src/view/Contact.js
@@ -1,107 +1,147 @@
 import React from 'react'
 import styled from 'styled-components'
 /**Mettre une condition si adresse mail valide ou non  */
-const FontStyle = styled.div `
-.container-form{
-  margin: 200px;
-  color:black;
-  background-color: white;
-  border-radius:5%;
-  width: 500px;
-  height: 720px;
 
-}
-form{
 
-  text-align:center;
-  display : flex;
-  justify-content : center;
-  font-size: 24px;
-  
-}
-input{
-  height: 30px;
-  width: 280px;
-}
-textarea{
-  
-  min-width : 200px;
- min-height : 200px;
- max-width : 390px;
- max-height : 220px;
-}
-label {
-  margin-top:10px;
-  display:block;
+
+
+
+export default function Contact() {
+  const FontStyle = styled.div `
+  .container-form{
+    margin: 200px;
+    color:black;
+    background-color: white;
+    border-radius:5%;
+    width: 500px;
+    height: 720px;
+    
   }
- label.inline {
-  display:inline;
-  margin-right:50px;
-  }
-  @media screen and (max-width : 873px){
-    .container-form{
-
-      width: 300px;
-      height: 520px;
-  
-  
-  }
-
   form{
-
-    font-size: 14px;
+    
+    text-align:center;
+    display : flex;
+    justify-content : center;
+    font-size: 24px;
     
   }
   input{
-    height: 10px;
-    width: 180px;
+    height: 30px;
+    width: 280px;
   }
-}
-
-
-
-
-`
-export default function Contact() {
-  return (
-    <FontStyle>
-
-<form name="contact" method="POST" data-netlify="true" onSubmit="submit">
-    <div className='container-form'>
+  textarea{
+    
+    min-width : 200px;
+    min-height : 200px;
+    max-width : 390px;
+    max-height : 220px;
+  }
+  label {
+    margin-top:10px;
+    display:block;
+  }
+  label.inline {
+    display:inline;
+    margin-right:50px;
+  }
+  @media screen and (max-width : 873px){
+    .container-form{
+      
+      width: 300px;
+      height: 520px;
+      
+      
+    }
+    
+    form{
+      
+      font-size: 14px;
+      
+    }
+    input{
+      height: 10px;
+      width: 180px;
+    }
+  }
   
-  <div>
-<label for ="name"> Prénom : </label>
-    <input type="text" id="firstname" name="user_firstname" placeholder='John' required/>
-  </div>
-  <div>
-<label for ="name"> Nom : </label>
-    <input type="text" id="name" name="user_name" placeholder='Doe' required/>
-  </div>
-  <div>
-
-  <label for="pet-select"> Votre demande concerne :</label>
-
-<select name="choice" id="coice-select" required>
-    <option value="">Merci de choisir parmis les choix suivant </option>
-    <option value="devis">Demande de devis</option>
-    <option value="particulier">Demande particulière</option>
-    <option value="rdv">Annulation de rdv</option>
-    <option value="retrait">Retrait de photo de la gallerie </option>
+  
+  
+  
+  `
+  
+  const encode = (data) => {
+    return Object.keys(data)
+    .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    .join("&");
+  }
+  
+  class ContactForm extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = { name: "", email: "", message: "" };
+    }
     
-</select>
-<label for ="mail"> E-mail:&nbsp;</label>
-    <input type="email" id="mail" name="user_mail" placeholder='exemple@email.com' required/>
-  </div>
-  <div>
-  <label for ="message"> Message : </label>
-    <textarea id="msg"  name="user_message" placeholder='Votre texte ici ...' required/>
-  </div>
-
+    /* Here’s the juicy bit for posting the form submission */
+    
+    handleSubmit = e => {
+      fetch("/", {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: encode({ "form-name": "contact", ...this.state })
+      })
+      .then(() => alert("Success!"))
+      .catch(error => alert(error));
+      
+      e.preventDefault();
+    };
+    
+    handleChange = e => this.setState({ [e.target.name]: e.target.value });
+    render ()
+    {
+      const { name, email, message } = this.state;
+      return (
+        <>
+      <FontStyle>
+      <form onSubmit={this.handleSubmit}>
+      
+      <div className='container-form'>
+      
+      <div>
+      <label for ="name"> Prénom : </label>
+      <input type="text" id="firstname" name="user_firstname" value={name} onChange={this.handleChange} placeholder='John' required/>
+      </div>
+      <div>
+      <label for ="name"> Nom : </label>
+      <input type="text" id="name" name="user_name" value={name} onChange={this.handleChange}  placeholder='Doe' required/>
+      </div>
+      <div>
+      
+      <label for="select"> Votre demande concerne :</label>
+      
+      <select name="choice" id="coice-select" required>
+      <option value="">Merci de choisir parmis les choix suivant </option>
+      <option value="devis">Demande de devis</option>
+      <option value="particulier">Demande particulière</option>
+      <option value="rdv">Annulation de rdv</option>
+      <option value="retrait">Retrait de photo de la gallerie </option>
+      
+      </select>
+      <label for ="mail"> E-mail:&nbsp;</label>
+      <input type="email" id="mail" name="user_mail"  value={email} onChange={this.handleChange} placeholder='exemple@email.com' required/>
+      </div>
+      <div>
+      <label for ="message"> Message : </label>
+      <textarea id="msg"  name="user_message"  value={message} onChange={this.handleChange} placeholder='Votre texte ici ...' required/>
+      </div>
+      
       <button type="submit" > Envoyé </button>
-    
-    </div>
-</form>
-    </FontStyle>
-
-  )
+      
+      </div>
+      </form>
+      </FontStyle>
+      
+      </>
+      )
+    }
+  }
 }
